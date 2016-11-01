35ecefc05d60283e681ebf3cce4c1a68beaebcd0
Cachette Creative Solutions <szarophoto@gmail.com>
2016-11-01 19:27:34 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,6 @@
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'rootpassword');
    define('DB_DATABASE', 'database');
+   #change server info, username and password BEFORE site goes live to ensure security.
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
